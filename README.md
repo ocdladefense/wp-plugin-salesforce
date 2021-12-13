cac81566e0e590d9b68af61d31aed01230f86758
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2021-12-13 13:29:39 -0800

    Bug fix

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # wp-plugin-salesforce
 
-
+## Installation
+1. Clone the repository or install using composer.
+2. Copy "examples/oauth-config-example.php" to "wp-content/oauth-config.php".
+3. In "oauth-config.php, set the constants and fill in the credentials.
+4. You should be good at this point.
--- examples/oauth-config-example.php
+++ examples/oauth-config-example.php
@@ -4,145 +4,31 @@
  * Change the name of this file and place it in wp-content/
  */
 
+define("SALESFORCE_DOMAIN", "https://test.salesforce.com");
+define("SALESFORCE_COMMUNITY_DOMAIN", "https://ocdpartial-ocdla.cs217.force.com");
+define("MY_DOMAIN", "http://localhost/wpocdla");
 
-function getConfig($configName){
-    
-    $configs = array(
-        "wp-admin" => array( 
-            "default" => true,
-            "sandbox" => true, // Might be used to determine domain for urls
-            "client_id" => "3MVG9gI0ielx8zHLKXlEe15aGYjrfRJ2j60D4kIpoTDqx2YSaK2xqoA3wU77thTRImxT5RSq_obv6EOQaZBm2",
-            "client_secret" => "3B61242366DCD4812DAA4C63A5FDF9C76F619528547B87A950A1584CEAB825E1",
-            "auth" => array(
-                "saml" => array(),
-                "oauth" => array(
-                    "usernamepassword" => array(
-                        "token_url" => "https://ltdglobal-customer.cs197.force.com/services/oauth2/token",
-                        "username" => "",
-                        "password" => "",
-                        "security_token" => ""
-                    ),
-                    "webserver" => array(
-                        "token_url" => "https://test.salesforce.com/services/oauth2/token",
-                        "auth_url" => "https://test.salesforce.com/services/oauth2/authorize",	// Web server ouath flow has two oauth urls.
-                        "redirect_url" => "http://localhost/oauth/api/request",
-                        "callback_url" => "http://localhost/ocdla-prod/my-account"
-                    )
-                )
-            )
-        ),
-        "wp-customer" => array(
-            "default" => true,
-            "sandbox" => true, // Might be used to determine domain for urls
-            "client_id" => "3MVG9gI0ielx8zHLKXlEe15aGYjrfRJ2j60D4kIpoTDqx2YSaK2xqoA3wU77thTRImxT5RSq_obv6EOQaZBm2",
-            "client_secret" => "3B61242366DCD4812DAA4C63A5FDF9C76F619528547B87A950A1584CEAB825E1",
-            "auth" => array(
-                "saml" => array(),
-                "oauth" => array(
-                    "usernamepassword" => array(
-                        "token_url" => "https://ltdglobal-customer.cs197.force.com/services/oauth2/token",
-                        "username" => "",
-                        "password" => "",
-                        "security_token" => ""
-                    ),
-                    "webserver" => array(
-                        "token_url" => "https://ocdpartial-ocdla.cs169.force.com/services/oauth2/token",
-                        "auth_url" => "https://ocdpartial-ocdla.cs169.force.com/services/oauth2/authorize",	// Web server ouath flow has two oauth urls.
-                        "redirect_url" => "http://localhost/oauth/api/request",
-                        "callback_url" => "http://localhost/ocdla-prod/my-account"
-                    )
-                )
-            )
-                    
-        )
-    );
-
-    return $configs[$configName];
-}
-
-
-
-
-
-
-
-if(!function_exists("salesforce_oauth_url_admin")) {
-
-	print "Hello world!";exit;
-function salesforce_oauth_url_admin(){
-
-    $clientId = "3MVG9gI0ielx8zHLKXlEe15aGYjrfRJ2j60D4kIpoTDqx2YSaK2xqoA3wU77thTRImxT5RSq_obv6EOQaZBm2";
-    $clientSecret = "3B61242366DCD4812DAA4C63A5FDF9C76F619528547B87A950A1584CEAB825E1";
-    $auth_url = "https://test.salesforce.com/services/oauth2/authorize";
-
-
-    $state = array("connected_app_name" => "ocdla sandbox", "flow" => "webserver");
-
-    $body = array(
-        "client_id"		=> $clientId,
-        //"redirect_uri"	=> "http://localhost/ocdla-prod/oauth/api/request",
-        "redirect_uri"	=> "http://localhost/ocdla-prod/my-account",
-        "response_type" => "code",
-        "state"         => json_encode($state)
-    );
-
-
-    $body = http_build_query($body);
-    return $auth_url."?".$body;
-}
-}
-
-
-if(!function_exists("salesforce_oauth_url_customer")) {
-function salesforce_oauth_url_customer(){
-    
-    $clientId = "3MVG9gI0ielx8zHLKXlEe15aGYjrfRJ2j60D4kIpoTDqx2YSaK2xqoA3wU77thTRImxT5RSq_obv6EOQaZBm2";
-    $clientSecret = "3B61242366DCD4812DAA4C63A5FDF9C76F619528547B87A950A1584CEAB825E1";
-    $auth_url = "https://ocdpartial-ocdla.cs169.force.com/services/oauth2/authorize";
-
-
-    $state = array("connected_app_name" => "ocdla sandbox", "flow" => "webserver");
-
-    $body = array(
-        "client_id"		=> $clientId,
-        //"redirect_uri"	=> "http://localhost/ocdla-prod/oauth/api/request",
-        "redirect_uri"	=> "http://localhost/ocdla-prod/my-account",
-        "response_type" => "code",
-        "state"         => json_encode($state)
-    );
-
-
-    $body = http_build_query($body);
-    return $auth_url."?".$body;
-}
-
-}
-
-
-
-
-################################# What Trevor is currently using #######################################################
 
 $oauth_config = array(
     "ocdla-wordpress" => array(
         "default" => true,
         "sandbox" => true, // Might be used to determine domain for urls
-        "client_id" => "3MVG9cb9UNjJbdEztW.V9X7_BWbACdRQT_VXR1Mf5RwIct5vTe71HEcQPJS9yKiFiwULW.KLFfmIUl_hg8rf7",
-        "client_secret" => "B91C4AD672BE55440C0A1C22D2F5FFB2D21357E82DF522D08DC2D198624CF20D",
+        "client_id" => "",
+        "client_secret" => "",
         "auth" => array(
             "saml" => array(),
             "oauth" => array(
                 "usernamepassword" => array(
-                    "token_url" => "https://test.salesforce.com/services/oauth2/token",
-                    "username" => "membernation@ocdla.com.ocdpartial",
-                    "password" => "asdi49ir4",
-                    "security_token" => "4te6Z194Uw4SHVHYut71NJvV"
+                    "token_url" => SALESFORCE_DOMAIN . "/services/oauth2/token",
+                    "username" => "",
+                    "password" => "",
+                    "security_token" => ""
                 ),
                 "webserver" => array(
-                    "token_url" => "https://ocdpartial-ocdla.cs217.force.com/services/oauth2/token",
-                    "auth_url" => "https://ocdpartial-ocdla.cs217.force.com/services/oauth2/authorize",
-                    "redirect_url" => "https://www.ocdla.org/sso-callback",
-                    "callback_url" => "https://localhost/jobs"
+                    "token_url" => SALESFORCE_COMMUNITY_DOMAIN . "/services/oauth2/token",
+                    "auth_url" => SALESFORCE_COMMUNITY_DOMAIN . "/services/oauth2/authorize",
+                    "redirect_url" => MY_DOMAIN . "/sso-callback",
+                    "callback_url" => MY_DOMAIN . "/jobs"
                 )
             )
         )
